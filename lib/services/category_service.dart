d366ba21fd03434182cf6844daa843d5a15df8de
Dizastaryo <159602209+Dizastaryo@users.noreply.github.com>
2025-04-27 19:45:00 +0500

    Проверка профvfvf

--- lib/services/category_service.dart
+++ lib/services/category_service.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import '../models/category.dart';
-import '../models/profile.dart';
 
 class CategoryService {
   final Dio _dio;
@@ -13,11 +12,5 @@ class CategoryService {
     return (response.data as List)
         .map((json) => Category.fromJson(json))
         .toList();
-  }
-
-  Future<Profile> getProfile() async {
-    final response =
-        await _dio.get('https://172.20.10.2:8000/categories/profile');
-    return Profile.fromJson(response.data);
   }
 }
